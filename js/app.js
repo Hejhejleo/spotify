04c106ebea08e140568d644b08e92ff84bf89312
Leonard Asp <Leoasp96@gmail.com>
2024-07-02 11:24:41 +0200

    Init

---
+++ js/app.js
@@ -0,0 +1,5 @@
+// This file can be used to add interactivity if needed
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('Document is ready!');
+  // Add any JavaScript interactivity here if needed
+});
